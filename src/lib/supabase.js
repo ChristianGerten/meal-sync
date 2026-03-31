f0b4242b7114609a9447b88b5ec1090f93b0b967
christiangerten <post@christian-gerten.de>
2026-03-31 23:05:47 +0200

    API Fix

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -11,5 +11,11 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
+  },
+  global: {
+    headers: {
+      'apikey': supabaseAnonKey,
+      'Authorization': `Bearer ${supabaseAnonKey}`
+    }
   }
 })
